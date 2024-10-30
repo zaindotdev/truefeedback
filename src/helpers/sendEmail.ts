c316d6dbbe894d459d312ac96825b2aaf8a78653
Muhammad Zain <zain683711@gmail.com>
2024-10-30 18:24:52 +0500

    fix bugs

--- src/helpers/sendEmail.ts
+++ src/helpers/sendEmail.ts
@@ -2,18 +2,32 @@ import { resend } from "@/lib/resend";
 import EmailTemplate from "@/components/email-template";
 
 import { ApiResponse } from "@/types/types";
+import { CreateEmailResponse } from "resend";
 
 export default async function sendVerificationEmail(email: string, username: string, verifyCode: string): Promise<ApiResponse> {
   try {
-    await resend.emails.send({
+    const response = await resend.emails.send({
       from: "onboarding@resend.dev",
       to: email,
       subject: "Verification Code",
       react: EmailTemplate({ username, otp: verifyCode })
-    })
-    return { success: true, message: "Email Sent Successfully" }
+    });
+
+    return {
+      success: true,
+      message: "Email Sent Successfully",
+      data: response as any,
+      isAcceptingMessage: true,
+      messages: [] // Assuming you have a `Message` type defined
+    };
   } catch (error) {
     console.log('Error sending verification email', error);
-    return { success: false, message: "Failed to send verification email" }
+    return {
+      success: false,
+      message: "Failed to send verification email",
+      data: error as any,
+      isAcceptingMessage: false,
+      messages: [] // Assuming you have a `Message` type defined
+    };
   }
 }
